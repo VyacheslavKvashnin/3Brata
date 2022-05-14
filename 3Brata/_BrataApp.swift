9ddb65af79ab870a655f4e9a28b7336aace89687
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-14 15:37:43 +0500

    Initial Commit

---
+++ 3Brata/_BrataApp.swift
@@ -0,0 +1,17 @@
+//
+//  _BrataApp.swift
+//  3Brata
+//
+//  Created by Вячеслав Квашнин on 14.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct _BrataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
